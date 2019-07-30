eeb24750a4ff9fdd6394ab13b1ffdb4980e28462
PolazhinetsA <32923196+PolazhinetsA@users.noreply.github.com>
2019-07-31 02:34:12 +0300

    Add files via upload

--- lzw/lzw.c
+++ lzw/lzw.c
@@ -21,35 +21,31 @@ int main(int argc, char **argv)
     }
 }
 
-typedef struct {
-    char *ptr;
-    int   len;
-} dent_t;
+enum { Prev, Suff };
 
 typedef struct {
-    dent_t *ent;
-    int     nent;
-    void   *htbl;
+    int (*ent)[2];
+    int   nent;
+    void *htbl;
 } dict_t;
 
 dict_t *dict_new();
 dict_t *dict_free(dict_t *);
-void dict_add(dict_t *, char *, int);
-int dict_find(dict_t *, char *, int);
+void dict_add(dict_t *, int, int);
+int dict_find(dict_t *, int, int);
 
 void encode(FILE *in, FILE *out)
 {
     dict_t *dict = dict_new();
     void *bout = bopen(out);
 
-    for (int ch = 0; ch != EOF; ) {
-        char buf[0x1000];
-        int len = 0;
-        do buf[len++] = ch = fgetc(in);
-            while (ch != EOF && -1 != dict_find(dict, buf, len));
-        ungetc((unsigned char )buf[--len], in);
-        bput(bout, dict_find(dict, buf, len), 12);
-        dict_add(dict, buf, len+1);
+    for (int prev, next, ch = 0; ch != EOF; ) {
+        next = -1;
+        do ch = fgetc(in); while (ch != EOF
+            && -1 != (next = dict_find(dict, prev = next, ch)));
+        bput(bout, prev, 12);
+        dict_add(dict, prev, ch);
+        ungetc(ch, in);
     }
     bflush(bout);
 
@@ -62,80 +58,75 @@ void decode(FILE *in, FILE *out)
     dict_t *dict = dict_new();
     void *bin = bopen(in);
 
-    for (int idx_ = -1, _idx; -1 != bget(bin, &_idx, 12); idx_ = _idx)
+    for (int prev = -1, suff, next; -1 != bget(bin, &next, 12); prev = next)
     {
-        dent_t *pent_ = &dict->ent[idx_],
-               *_pent = &dict->ent[_idx];
+        int add = next == dict->nent;
+        if (add) dict_add(dict, prev, suff);
 
-        if (idx_ != -1 && dict->nent < 0x1000) {
-            char buf[0x1000];
-            memcpy(buf, pent_->ptr, pent_->len);
-            buf[pent_->len] = _pent->ptr ? *_pent->ptr : *pent_->ptr;
-            dict_add(dict, buf, pent_->len+1);
-        }
-        fwrite(_pent->ptr, 1, _pent->len, out);
+        unsigned char buf[0x1000];
+        int pos = 0x1000, len = 0, curr = next;
+        do buf[--pos] = dict->ent[curr][Suff], ++len;
+            while(-1 != (curr = dict->ent[curr][Prev]));
+
+        fwrite(buf+pos, 1, len, out);
+        suff = buf[pos];
+        if (!add && prev != -1) dict_add(dict, prev, suff);
     }
 
     free(bin);
     dict_free(dict);
 }
 
-
-int enthash(dent_t *, int);
-int entcmp(dent_t *, dent_t *);
+int enthash(void *, int);
+int entcmp(int *, int *);
 
 dict_t *dict_new()
 {
     dict_t *this = malloc(sizeof(dict_t));
-    this->ent = calloc(0x1000, sizeof(dent_t));
+    this->ent = calloc(0x1000, 2 * sizeof(int));
     this->nent = 0;
-    this->htbl = htbl_new(2003, enthash, entcmp);
+    this->htbl = htbl_new(5003, enthash, entcmp);
 
-    char ch = '\0';
-    do dict_add(this, &ch, 1); while (++ch);
+    unsigned char ch = 0;
+    do dict_add(this, -1, ch); while (++ch);
 
     return this;
 }
 
 dict_t *dict_free(dict_t *this)
 {
-    for (int i = 0; i < 0x1000; ++i) {
-        if (!this->ent[i].ptr) break;
-        free(this->ent[i].ptr);
-    }
     htbl_free(this->htbl, NULL);
     free(this->ent);
     free(this);
 }
 
-void dict_add(dict_t *this, char *ptr, int len)
+void dict_add(dict_t *this, int prev, int suff)
 {
     if (this->nent == 0x1000) return;
 
-    memcpy(this->ent[this->nent].ptr = malloc(len), ptr,
-           this->ent[this->nent].len = len);
-    htbl_add(this->htbl, &this->ent[this->nent++]);
+    this->ent[this->nent][Prev] = prev;
+    this->ent[this->nent][Suff] = suff;
+    htbl_add(this->htbl, this->ent[this->nent++]);
 }
 
-int dict_find(dict_t *this, char *ptr, int len)
+int dict_find(dict_t *this, int prev, int suff)
 {
-    dent_t ent;
-    ent.ptr = ptr;
-    ent.len = len;
+    int ent[2];
+    ent[Prev] = prev;
+    ent[Suff] = suff;
 
-    dent_t *pent = htbl_find(this->htbl, &ent);
-    return pent ? pent - this->ent : -1;;
+    int (*pent)[2] = htbl_find(this->htbl, ent);
+    return pent ? pent - this->ent : -1;
 }
 
-int enthash(dent_t *ent, int rng)
+int enthash(void *ent, int rng)
 {
-    return memhash(ent->ptr, ent->len, rng);
+    return memhash(ent, 2 * sizeof(int), rng);
 }
 
-int entcmp(dent_t *ent1, dent_t *ent2)
+int entcmp(int *ent1, int *ent2)
 {
-    int dl = ent1->len - ent2->len,
-        ret = memcmp(ent1->ptr, ent2->ptr, dl < 0 ? ent1->len : ent2->len);
-
-    return ret ? ret : dl;
+    return ent1[Prev] - ent2[Prev]
+           ? ent1[Prev] - ent2[Prev]
+           : ent1[Suff] - ent2[Suff];
 }
