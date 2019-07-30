07f6714cc6265d91da49a23ab0197049b08a8b84
PolazhinetsA <32923196+PolazhinetsA@users.noreply.github.com>
2019-07-30 15:36:07 +0300

    Update radixsort.c

--- radixsort.c
+++ radixsort.c
@@ -6,40 +6,36 @@
  * k - key extractor.
  */
 #define rsort_macro(a,b,c,n,r,k) {      \
-    int _t[r],                          \
-        *_c = (c) ? (c) : _t;           \
+    int _t[r], *_c = (c) ? (c) : _t;    \
     memset(_c, 0, sizeof(_t));          \
                                         \
     for (int _i = 0; _i < (n); ++_i) {  \
         ++_c[k((a)[_i])];               \
-    }                                   \
     for (int _i = 1; _i < (r); ++_i) {  \
         _c[_i] += _c[_i-1];             \
-    }                                   \
     for (int _i = n-1; _i >= 0; --_i) { \
         (b)[--_c[k((a)[_i])]] = (a)[_i];\
-    }                                   \
 }
 #define bits(x,b_,_b) (*(unsigned long long *)&(x) >> (b_) & ((1 << (_b)) - 1))
 #define swap(a,b) { typeof(a) _t = a; a = b; b = _t; }
- 
+
 /* count sort by least significant byte 0..7,
  * then by 8..15 and so on...
  */
 void rsort_int(int *arr, int num)
 {
     int *arr_ = malloc(num * sizeof(int));
- 
+
     for (int shr = 0; shr < sizeof(int) * 8; shr += 8) {
         #define key(x) bits(x,shr,8)
         rsort_macro(arr, arr_, NULL, num, 0x100, key);
         #undef key
         swap(arr, arr_);
     }
- 
+
     free(arr_);
 }
- 
+
 /* radix sorting of double precision floating point numbers:
  * 1. by mantissa, just like integers;
  * 2. by exponent, just like integers;
@@ -53,7 +49,7 @@ void rsort_int(int *arr, int num)
 void rsort_dbl(double *arr, int num)
 {
     double *arr_ = malloc(num * sizeof(double));
- 
+
     for (int shr = 0; shr < 52; shr += 13) {
         #define key(x) bits(x,shr,13)
         rsort_macro(arr, arr_, NULL, num, 0x2000, key);
@@ -64,16 +60,16 @@ void rsort_dbl(double *arr, int num)
     rsort_macro(arr, arr_, NULL, num, 0x800, key);
     #undef key
     swap(arr, arr_);
- 
+
     for (int i = num-1, i_ = 0, _i = num-1; i >= 0; --i) {
         if (arr[i] < 0) arr_[i_++] = arr[i];
                    else arr_[_i--] = arr[i];
     }
     swap(arr, arr_);
- 
+  
     free(arr_);
 }
- 
+
 /* sorting of strings:
  * 1. find maxlen
  * 2. sort by len
@@ -92,7 +88,7 @@ void rsort_str(unsigned char **arr, int num)
         size_t         len;
     } *arr_  = malloc(num * sizeof(struct str)),
       *arr__ = malloc(num * sizeof(struct str));
- 
+
     int maxl = 0;
     for (int i = 0; i < num; ++i) {
         arr_[i].ptr = arr[i];
@@ -100,7 +96,7 @@ void rsort_str(unsigned char **arr, int num)
         if (maxl < arr_[i].len)
             maxl = arr_[i].len;
     }
- 
+
     /* for l=0..maxl: lidx[l] will become the index of the first string not
      * shorter than l, when used for counting. Passing decremented pointer
      * would do the trick, if there was guarantee, that no zero-length strings
@@ -111,7 +107,7 @@ void rsort_str(unsigned char **arr, int num)
     rsort_macro(arr_, arr__, lidx, num, maxl+1, key);
     #undef key
     swap(arr_, arr__);
- 
+
     for (int l = maxl, i = l-1; l > 0; --l, --i) {
         memcpy(arr__, arr_, lidx[l] * sizeof(struct str));
         #define key(x) ((x).ptr[i])
@@ -120,7 +116,7 @@ void rsort_str(unsigned char **arr, int num)
         #undef key
         swap(arr_, arr__);
     }
- 
+
     for (int i = 0; i < num; ++i) {
         arr[i] = arr_[i].ptr;
     }
