aca391d064c58103f9ac8c413cca6d18c0c3397f
PolazhinetsA <32923196+PolazhinetsA@users.noreply.github.com>
2019-07-31 18:52:39 +0300

    Add files via upload

--- lzw/bitio.c
+++ lzw/bitio.c
@@ -1,6 +1,6 @@
 #include "bitio.h"
 
-#define uintwid (sizeof(unsigned) * 8)
+#define UINTWID (sizeof(unsigned) * 8)
 
 typedef struct {
     FILE *file;
@@ -13,7 +13,7 @@ void *bopen(FILE *file)
     bfile_t *this = malloc(sizeof(bfile_t));
     this->file = file;
     this->ibuf = 0;
-    this->ipos = uintwid;
+    this->ipos = UINTWID;
     this->obuf = 0;
     this->opos = 0;
 }
@@ -22,24 +22,24 @@ int bget(void *_this, unsigned *pbits, unsigned nbits)
 {
     bfile_t *this = _this;
 
-    nbits = nbits <= uintwid ? nbits : uintwid;
-    *pbits = (this->ipos < uintwid)
-           ? (this->ibuf >> this->ipos) & ((unsigned )-1 >> (uintwid - nbits))
+    nbits = nbits <= UINTWID ? nbits : UINTWID;
+    *pbits = (this->ipos < UINTWID)
+           ? (this->ibuf >> this->ipos) & ((unsigned )-1 >> (UINTWID - nbits))
            : 0;
     this->ipos += nbits;
-    if (this->ipos <= uintwid)
+    if (this->ipos <= UINTWID)
         return 0;
 
     this->ibuf = 0;
-    this->ipos -= uintwid;
+    this->ipos -= UINTWID;
     unsigned rcnt = 8 * fread(&this->ibuf, 1, sizeof(unsigned), this->file);
     if (rcnt < this->ipos)
         return -1;
 
-    *pbits |= (this->ibuf & ((unsigned )-1 >> (uintwid - this->ipos)))
+    *pbits |= (this->ibuf & ((unsigned )-1 >> (UINTWID - this->ipos)))
               << (nbits - this->ipos);
-    this->ibuf <<= uintwid - rcnt;
-    this->ipos += uintwid - rcnt;
+    this->ibuf <<= UINTWID - rcnt;
+    this->ipos += UINTWID - rcnt;
         return 0;
 }
 
@@ -47,15 +47,15 @@ void bput(void *_this, unsigned bits, unsigned nbits)
 {
     bfile_t *this = _this;
 
-    nbits = nbits <= uintwid ? nbits : uintwid;
-    if (nbits != uintwid) bits &= (1 << nbits) - 1;
+    nbits = nbits <= UINTWID ? nbits : UINTWID;
+    if (nbits != UINTWID) bits &= (1 << nbits) - 1;
     this->obuf |= bits << this->opos;
     this->opos += nbits;
-    if (this->opos < uintwid)
+    if (this->opos < UINTWID)
         return;
 
     fwrite(&this->obuf, sizeof(unsigned), 1, this->file);
-    this->opos -= uintwid;
+    this->opos -= UINTWID;
     this->obuf = this->opos ? bits >> (nbits - this->opos) : 0;
 }
 
